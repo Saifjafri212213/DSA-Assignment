272a29a53f93721e14c361eb896c963923bdbe06
Saifjafri212213 <131288625+Saifjafri212213@users.noreply.github.com>
2026-06-15 22:44:04 +0530

    Create 2.cpp

---
+++ DAY2/2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+        bool hasCycle(ListNode *head) {
+        ListNode* tortoise= head;
+        ListNode* rabbit= head;
+
+        while(rabbit!=NULL && rabbit->next!=NULL){
+            tortoise= tortoise->next;
+            rabbit= rabbit->next->next;
+
+            if(rabbit==tortoise){
+                return true;
+            }
+        }
+        return false;
+
+    }
+};
